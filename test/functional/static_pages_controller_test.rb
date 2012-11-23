bd77b3da57fcb8c76e99fccd0321f634074f5f2e
Andrew Harris <sirrahd@gmail.com>
2012-11-23 09:06:54 -0500

    Created StaticPages controller, added some placeholder text using I18n, and simple title test

--- test/functional/static_pages_controller_test.rb
+++ test/functional/static_pages_controller_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
-    assert_response :success
+    assert_response :success, "Home did not load"
+	assert_select 'title', "TeacherMaps", "Home title not correct"
   end
 
 end
