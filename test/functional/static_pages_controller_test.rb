df76fb1d1e201adc81b17e7a4081690f50732351
Andrew Harris <sirrahd@gmail.com>
2013-01-12 23:29:17 -0500

    Fixed some testing false flags. Testing needs work.

--- test/functional/static_pages_controller_test.rb
+++ test/functional/static_pages_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
-    assert_response :success, "Home did not load"
+    assert_response :redirect, "Home did not load"
   end
 
   test "should get help" do
