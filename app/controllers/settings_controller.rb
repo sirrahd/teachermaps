46f163a87df7fc7ae23c8a02af3f3d8176e3d1a2
James Kobol <jim.kobol@gmail.com>
2013-02-02 01:14:44 -0700

    update pushFlash javascript to allow more flexible parameters, for example, I added the fadeOut parameter which accepts the milliseconds to keep the flash visible.

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -48,43 +48,18 @@ class SettingsController < ApplicationController
     @setting = Setting.find(params[:id])
   end
 
-  # # POST /settings
-  # # POST /settings.json
-  # def create
-  #   @setting = Setting.new(params[:setting])
-
-  #   respond_to do |format|
-  #     if @setting.save
-  #       format.html { redirect_to @setting, notice: 'Setting was successfully created.' }
-  #       format.json { render json: @setting, status: :created, location: @setting }
-  #     else
-  #       format.html { render action: "new" }
-  #       format.json { render json: @setting.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
   # PUT /settings/1
   # PUT /settings/1.json
   def update
     @setting = Setting.find(params[:id])
 
     respond_to do |format|
-      # if @setting.update_attributes(params[:setting])
-      #   format.html { redirect_to @setting, notice: 'Setting was successfully updated.' }
-      #   format.json { head :no_content }
-      # else
-      #   format.html { render action: "edit" }
-      #   format.json { render json: @setting.errors, status: :unprocessable_entity }
-      # end
       if @setting.update_attributes(params[:setting])
-        # format.html { render :partial => 'resources/resources_table' }
         Rails.logger.info("Settings save success")
         format.html { render :nothing => true, :status => 200, :content_type => 'text/html' }
         format.js
       else
         Rails.logger.info("Settings save error")
-        #format.html { render :partial => 'resources/error_messages', :error => true, :status => 500  }
         format.html { render :nothing => true, :status => 500, :content_type => 'text/html' }
         format.js
       end
