860608b88f8b7aea6a7650d440564da9ad0fe15c
James Kobol <jim.kobol@gmail.com>
2013-02-03 09:22:47 -0700

    I think I finally got form data + file data to be submitted via ajax.

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Teachermaps::Application.routes.draw do
 
   # Sync Google Drive and/or DropBox resources
   match 'resources/sync' => 'resources#sync', :as => 'sync_resources'
+  match '/resources/ajax/file' => 'resources#ajax_upload_file'
   resources :resources
+  
   
 
   resources :users
