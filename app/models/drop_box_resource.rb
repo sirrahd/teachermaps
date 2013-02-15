d93f4c314da86c76686fcff1245dd5811c577d1a
James Kobol <jim.kobol@gmail.com>
2013-02-15 10:35:19 -0700

    wrapped dropbox link query in a while loop to dropbox

--- app/models/drop_box_resource.rb
+++ app/models/drop_box_resource.rb
@@ -9,10 +9,13 @@ class DropBoxResource < Resource
 	#TODO: Refactor this to be called when crated and cache in db
 	def open_link()
 		
-		if self.link?
+		safe_count = 0
+		while self.link.nil? && safe_count < 3
 			Rails.logger.info("Could not find link")
 			drop_box_account = DropBoxAccount.find_by_user_id( self.user_id )
 			self.link = drop_box_account.media_link( self.path )
+			# We do not want this to hang
+			safe_count += 1
 		end
 		Rails.logger.info("Found link #{self.link}")
 		self.link		
