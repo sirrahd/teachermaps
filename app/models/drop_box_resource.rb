13431e6364652c18a997e4b4af25c8c54080602d
James Kobol <jim.kobol@gmail.com>
2013-02-15 10:51:07 -0700

    fixed bootstrap backdrop error on second submit

--- app/models/drop_box_resource.rb
+++ app/models/drop_box_resource.rb
@@ -6,18 +6,17 @@ class DropBoxResource < Resource
 
 	belongs_to :user
 
-	#TODO: Refactor this to be called when crated and cache in db
 	def open_link()
 		
-		safe_count = 0
-		while self.link.nil? && safe_count < 3
-			Rails.logger.info("Could not find link")
-			drop_box_account = DropBoxAccount.find_by_user_id( self.user_id )
-			self.link = drop_box_account.media_link( self.path )
-			# We do not want this to hang
-			safe_count += 1
-		end
-		Rails.logger.info("Found link #{self.link}")
+		# safe_count = 0
+		# while self.link.nil? && safe_count < 3
+		# 	Rails.logger.info("Could not find link")
+		# 	drop_box_account = DropBoxAccount.find_by_user_id( self.user_id )
+		# 	self.link = drop_box_account.media_link( self.path )
+		# 	# We do not want this to hang
+		# 	safe_count += 1
+		# end
+		Rails.logger.info("Using link #{self.link}")
 		self.link		
 	end
 end
