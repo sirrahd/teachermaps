61d6b315d9984073e2248d8623b30df0e7c29b3a
James Kobol <jim.kobol@gmail.com>
2013-02-15 02:26:30 -0700

    added print statements

--- app/models/drop_box_resource.rb
+++ app/models/drop_box_resource.rb
@@ -10,10 +10,11 @@ class DropBoxResource < Resource
 	def open_link()
 		
 		if self.link?
+			Rails.logger.info("Could not find link")
 			drop_box_account = DropBoxAccount.find_by_user_id( self.user_id )
 			self.link = drop_box_account.media_link( self.path )
 		end
-
+		Rails.logger.info("Found link #{self.link}")
 		self.link		
 	end
 end
