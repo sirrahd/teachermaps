1474f9f742e9b3c670017c6f1cea8525ead7e830
James Kobol <jim.kobol@gmail.com>
2013-02-18 22:04:55 -0700

    get funky!!

--- app/models/mime_type.rb
+++ app/models/mime_type.rb
@@ -1,4 +1,25 @@
 class MimeType < ActiveRecord::Base
+
+   TYPES = {
+    'text/html' => 'HTML',
+    'text/link' => 'Web',
+    'text/plain'=> 'Text',
+    'application/rtf'=> 'Text',
+    'application/vnd.oasis.opendocument.text'=> 'Word Document',
+    'application/pdf'=> 'PDF',
+    'application/msword' => 'Word Document',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document' => 'Word Document',
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' => 'Spreadsheet',
+    'application/x-vnd.oasis.opendocument.spreadsheet' => 'Spreadsheet',
+    'image/jpeg'=> 'Image',
+    'image/gif'=> 'Image',
+    'image/png'=> 'Image',
+    'image/svg+xml'=> 'Image',
+    'application/vnd.openxmlformats-officedocument.presentationml.presentation' => 'Presentation'
+  }
+
+
+  
   # attr_accessible :title, :body
-  attr_accessible :name, :mimetype, :thumb_nail
+  attr_accessible :name, :mime_type, :thumb_nail
 end
