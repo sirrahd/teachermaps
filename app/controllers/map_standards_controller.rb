7adfeaa2222bbe987e2ed5597f42a7548a23bc49
James Kobol <jim.kobol@gmail.com>
2013-03-25 16:35:56 -0600

    added auto-update to map standards

--- app/controllers/map_standards_controller.rb
+++ app/controllers/map_standards_controller.rb
@@ -11,39 +11,37 @@ class MapStandardsController < ApplicationController
 
     print params
 
-    map = Map.find_by_id_and_user_id(params[:map_id], @current_user.id)
+    @map = Map.find_by_id_and_user_id(params[:map_id], @current_user.id)
     standard = Standard.find(params[:standard_id])
 
-    if !map or !standard
+    if !@map or !standard
       Rails.logger.info("Could not locate either map #{params[:map_id]} or standard #{params[:standard_id]}") 
       return render :nothing => true, :status => 404
     end
 
-    Rails.logger.info("Located Map #{map} and Standard #{standard}")
+    Rails.logger.info("Located Map #{@map} and Standard #{standard}")
 
-    if !MapStandard.find_by_standard_id_and_map_id_and_user_id(standard.id, map.id, @current_user.id)
+    if !MapStandard.find_by_standard_id_and_map_id_and_user_id(standard.id, @map.id, @current_user.id)
         new_map_standard = MapStandard.new
         new_map_standard.standard = standard
-        new_map_standard.map = map
+        new_map_standard.map = @map
         new_map_standard.user = @current_user
-        map.map_standards << new_map_standard
+        @map.map_standards << new_map_standard
     end
 
     # Add any children standards
     standard.children_standards.each do |child_standard|
       # Enforce prevention of the same standard being added twice
-      if !MapStandard.find_by_standard_id_and_map_id_and_user_id(child_standard.id, map.id, @current_user.id)
+      if !MapStandard.find_by_standard_id_and_map_id_and_user_id(child_standard.id, @map.id, @current_user.id)
         new_map_standard = MapStandard.new
         new_map_standard.standard = child_standard
-        new_map_standard.map = map
+        new_map_standard.map = @map
         new_map_standard.user = @current_user
-        map.map_standards << new_map_standard
+        @map.map_standards << new_map_standard
       end
     end
 
-    respond_to do |format|
-      format.json { render json: '', :status => :ok}
-    end
+    return render partial: 'maps/map_standards_list'
   end
 
   def ajax_destroy
@@ -56,11 +54,11 @@ class MapStandardsController < ApplicationController
 
     print params
 
-    map = Map.find_by_id_and_user_id(params[:map_id], @current_user.id)
+    @map = Map.find_by_id_and_user_id(params[:map_id], @current_user.id)
     standard = Standard.find(params[:standard_id])
-    map_standard = MapStandard.find_by_standard_id_and_map_id_and_user_id(params[:standard_id], params[:map_id], @current_user.id)
+    map_standard = MapStandard.find_by_standard_id_and_map_id_and_user_id(params[:standard_id], @map.id, @current_user.id)
 
-    if !map or !standard or !map_standard
+    if !@map or !standard or !map_standard
       Rails.logger.info("Could not locate either map standard from given data") 
       return render :nothing => true, :status => 404
     end
@@ -69,9 +67,7 @@ class MapStandardsController < ApplicationController
 
     Rails.logger.info("Deleted map standard")
 
-    respond_to do |format|
-      format.json { render json: '', :status => :ok}
-    end
+    return render partial: 'maps/map_standards_list'
   end
 
   private 
--- app/models/map_standard.rb
+++ app/models/map_standard.rb
@@ -15,8 +15,6 @@ class MapStandard < ActiveRecord::Base
   validates :user, presence: true
   validates :map, presence: true
   validates_uniqueness_of :slug, allow_nil: true, case_sensitive: true
-  #validates_uniqueness_of :teacher_id, :scope => [:semester_id, :class_id]
-  validates_uniqueness_of :user_id, :scope => [:user_id, :standard_id, :user_id]
 
   before_create :default_values
 
