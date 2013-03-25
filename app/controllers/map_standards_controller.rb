06984c4e52fa1282805b7693bb631b67c2ad02d6
James Kobol <jim.kobol@gmail.com>
2013-03-25 16:01:18 -0600

    can add parent and children of standards, can remove standards from a Map via ajax

--- app/controllers/map_standards_controller.rb
+++ app/controllers/map_standards_controller.rb
@@ -8,9 +8,6 @@ class MapStandardsController < ApplicationController
     if !@current_user 
       return render :nothing => true, :status => 403
     end
-    if !params.has_key?('map_id') or !params.has_key?('standard_id')
-      return render :nothing => true, :status => 404
-    end
 
     print params
 
@@ -24,15 +21,23 @@ class MapStandardsController < ApplicationController
 
     Rails.logger.info("Located Map #{map} and Standard #{standard}")
 
-    map.map_standards.each do |map_standard|
-      # Enforce prevention of the same standard being added twice
-      if map_standard.standard.id != standard.id
+    if !MapStandard.find_by_standard_id_and_map_id_and_user_id(standard.id, map.id, @current_user.id)
         new_map_standard = MapStandard.new
         new_map_standard.standard = standard
         new_map_standard.map = map
         new_map_standard.user = @current_user
         map.map_standards << new_map_standard
-        break
+    end
+
+    # Add any children standards
+    standard.children_standards.each do |child_standard|
+      # Enforce prevention of the same standard being added twice
+      if !MapStandard.find_by_standard_id_and_map_id_and_user_id(child_standard.id, map.id, @current_user.id)
+        new_map_standard = MapStandard.new
+        new_map_standard.standard = child_standard
+        new_map_standard.map = map
+        new_map_standard.user = @current_user
+        map.map_standards << new_map_standard
       end
     end
 
@@ -42,12 +47,27 @@ class MapStandardsController < ApplicationController
   end
 
   def ajax_destroy
-    if !@current_user
+    if !@current_user 
       return render :nothing => true, :status => 403
     end
+    if !params.has_key?('map_id') or !params.has_key?('standard_id')
+      return render :nothing => true, :status => 404
+    end
+
     print params
 
+    map = Map.find_by_id_and_user_id(params[:map_id], @current_user.id)
+    standard = Standard.find(params[:standard_id])
+    map_standard = MapStandard.find_by_standard_id_and_map_id_and_user_id(params[:standard_id], params[:map_id], @current_user.id)
 
+    if !map or !standard or !map_standard
+      Rails.logger.info("Could not locate either map standard from given data") 
+      return render :nothing => true, :status => 404
+    end
+
+    map_standard.destroy
+
+    Rails.logger.info("Deleted map standard")
 
     respond_to do |format|
       format.json { render json: '', :status => :ok}
