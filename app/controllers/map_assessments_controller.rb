82ce10a4e6d5e09dfeecbf6e137df0cee2b7472c
James Kobol <jim.kobol@gmail.com>
2013-03-30 14:55:55 -0600

    can add and delete unit assessments via ajax

--- app/controllers/map_assessments_controller.rb
+++ app/controllers/map_assessments_controller.rb
@@ -1,16 +1,20 @@
 class MapAssessmentsController < ApplicationController
+	include SessionsHelper
 
+	before_filter :require_session
 
 	def create
 		Rails.logger.info(params)
 	    @map_assessment = MapAssessment.new(params[:map_assessment])
 	    @map = Map.find(params[:map_assessment][:map_id])
-	    @map.map_assessments << @map_assessments
+	    @map.map_assessments << @map_assessment
 
 	    respond_to do |format|
 	      if @map_assessment.save and @map.save
+	      	Rails.logger.info("Success!!!")
 	      	format.html { render :partial => 'maps/list_map_assessments', :locals => { :object => @map } }
 	      else
+	      	Rails.logger.info("Failure!!!")
 	      	format.html { render :json => @map_assessment.errors, :status => :unprocessable_entity  }
 	      end
 	    end
@@ -28,6 +32,36 @@ class MapAssessmentsController < ApplicationController
 		      	format.json { respond_with_bip(@map_assessment) }
 		    end
   		end
+  	end
+
+
+  	def destroy
+
+  		@map_assessment = MapAssessment.find_by_id_and_user_id( params[:id], @current_user.id)
+
+  		return render nothing: true, status: 404 if !@map_assessment
+
+	    # Cache for flash notification
+	    deleted_title = @map_assessment.name 
+	    @map = @map_assessment.map
+
+	    # Removing resource
+	    @map_assessment.destroy
+
+	    respond_to do |format|
+	      if @map.save
+	      	format.html { render :partial => 'maps/list_map_assessments', :locals => { :object => @map } }
+	      else
+	      	format.html { render nothing: true, status: 500 }
+	      end
+	    end
+  	end 
+
+  	# Requires user session
+  	def require_session
+    	unless current_user
+      		redirect_to signin_path
+    	end
   	end
 
 end
