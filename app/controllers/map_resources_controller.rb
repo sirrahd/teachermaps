3eb6a7f9c5a9da6b1bebf32ce266e56f9fe14ae2
James Kobol <jim.kobol@gmail.com>
2013-04-08 15:17:37 -0600

    restricted map resource editing to the user currently logged in.

--- app/controllers/map_resources_controller.rb
+++ app/controllers/map_resources_controller.rb
@@ -1,17 +1,27 @@
 class MapResourcesController < ApplicationController
+	include SessionsHelper
+
+	before_filter :require_session
 
 	def update
-		@map_resource = MapResource.find params[:id]
+		@map_resource = MapResource.find_by_id_and_user_id params[:id], @current_user.id
 
-	  	respond_to do |format|
-	    	if @map_resource.update_attributes(params[:map_resource])
-	      		format.html { redirect_to(@map_resource, :notice => 'MapResource was successfully updated.') }
-		      	format.json { respond_with_bip(@map_resource) }
-		    else
-		      	format.html { render :action => "edit" }
-		      	format.json { respond_with_bip(@map_resource) }
-		    end
-  		end
-  	end
+  	respond_to do |format|
+    	if @map_resource.update_attributes params[:map_resource] 
+	      	format.json { respond_with_bip(@map_resource) }
+	    else
+	      	format.json { respond_with_bip(@map_resource) }
+	    end
+		end
+	end	
+
+	private 
+  
+  # Requires user session
+  def require_session
+    unless current_user
+      redirect_to signin_path
+    end
+  end
 
 end
