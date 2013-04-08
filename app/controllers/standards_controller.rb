0c6b85c505599d76d61f831e967621a58008d5c9
James Kobol <jim.kobol@gmail.com>
2013-04-08 17:00:46 -0600

    refactored standards

--- app/controllers/standards_controller.rb
+++ app/controllers/standards_controller.rb
@@ -4,7 +4,7 @@ class StandardsController < ApplicationController
   before_filter :require_session
 
   def ajax_filter
-    Rails.logger.info("Filter Params: #{params}")
+    Rails.logger.info(params)
 
     if !params.has_key?('standard_type') or !params.has_key?('course_subject')
       Rails.logger.info("Dude, you forgot the standard type or course subject #{params[:course_subject]} #{params[:standard_type]}")
@@ -12,28 +12,25 @@ class StandardsController < ApplicationController
     end
 
     @map = Map.find(params[:map_id])
-    @standards = Standard.find(:all, :conditions=>{:standard_type_id=>params[:standard_type], :parent_standard_id=>nil})
+    @standards = Standard.find(:all, conditions: {standard_type_id: params[:standard_type], parent_standard_id: nil})
 
     if params.has_key?('course_subject')
-      @standards &= Standard.find(:all, :conditions=>{:course_subject_id=>params[:course_subject]})
+      @standards &= Standard.find(:all, conditions: {course_subject_id: params[:course_subject]})
     end
   
     if params.has_key?('course_grades')
-      @standards &= Standard.find(:all, :joins => :course_grades, :conditions=>{:course_grades=>{:id => params[:course_grades]}})
+      @standards &= Standard.find(:all, joins: :course_grades, conditions: {course_grades: {id: params[:course_grades]}})
     end
 
     if params.has_key?('q') and !params[:q].empty?
-      #@standards &= Standard.where( Standard.arel_table[:text].matches("%#{params[:q].strip}%") )
-      @standards &= Standard.find(:all, :conditions => ['text LIKE ?', "%#{params[:q].strip}%"])
+      @standards &= Standard.find(:all, conditions: ['text LIKE ?', "%#{params[:q].strip}%"])
     end
     
     if @current_user
-      map_standards = MapStandard.where(user_id: @current_user.id, map_id: params[:map_id])
-      @map_standards_by_standard_id = Hash[map_standards.map { |p| [p['standard_id'], p] }]
-      # Rails.logger.info("Map Standards: #{@map_standards_by_standard_id.inspect}")
+      @map_standards = MapStandard.where(user_id: @current_user.id, map_id: params[:map_id])
+      @map_standards_by_standard_id = Hash[@map_standards.map { |p| [p['standard_id'], p] }]
     end
-
-    # Rails.logger.info(@standards);
+    
     render :partial => 'maps/list_standards'
   end
 
