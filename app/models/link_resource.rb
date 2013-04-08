14fbcf0aa901e66a6675db7df7c458db5dacf0fb
James Kobol <jim.kobol@gmail.com>
2013-04-08 16:29:43 -0600

    working on resource create link

--- app/models/link_resource.rb
+++ app/models/link_resource.rb
@@ -1,13 +1,15 @@
 class LinkResource < Resource
 
 	TYPE = 'LinkResource'
+
+  attr_accessible :title, :link
 	
 	belongs_to :user
 
-  	attr_accessible :link
+  attr_accessible :link
 
-  	validates :link, :presence => { :message => I18n.t('link_resources.link_blank_error') }, :length => { :minimum => 2, :maximum => 2048 }
-  	validates_format_of :link, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix, :message =>  I18n.t('link_resources.link_not_valid')
+  validates :link, :presence => { :message => I18n.t('link_resources.link_blank_error') }, :length => { :minimum => 2, :maximum => 2048 }
+  validates_format_of :link, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix, :message =>  I18n.t('link_resources.link_not_valid')
   	
   def open_link()
 		self.link
