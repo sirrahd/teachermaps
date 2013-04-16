f1b198c8eb6665b7c2f7be45b067a65a2761eafe
James Kobol <jim.kobol@gmail.com>
2013-04-15 22:34:32 -0600

    added function to normalize ranges of grades; it hurts to look at it….so…yeah

--- app/models/map.rb
+++ app/models/map.rb
@@ -150,6 +150,42 @@ class Map < ActiveRecord::Base
     end
   end
 
+  def course_grade_ranges
+
+
+    return '' if self.course_grades.empty?
+
+    grades = self.course_grades.map do |grade|
+      if grade.name == 'K'
+        0
+      else 
+        Integer(grade.name)
+      end
+    end
+
+    ranges = grades.sort.uniq.inject([]) do |spans, n|
+      Rails.logger.info("#{spans} #{n}")
+      if spans.empty? || spans.last.last.succ != n
+        Rails.logger.info("\t#{spans.last} #{n}")
+        spans + [n..n]
+      else
+        spans[0..-2] + [spans.last.first..n]
+      end
+    end
+
+    Rails.logger.info(ranges)
+    result = []
+    ranges.each do |range|
+      if range.first != range.last
+        result << "#{range.first}&mdash;#{range.last}"
+      elsif range.first == range.last and range.first
+        result << "#{range.first}"
+      end
+    end
+
+    result.join(', ').sub('0', 'K')
+  end
+
   private 
 
   def clean_attrs
