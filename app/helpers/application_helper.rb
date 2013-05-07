29c408fa896cefa69e36a88a90558cbc483b8bb6
James Kobol <jim.kobol@gmail.com>
2013-05-07 13:22:59 -0600

    forgot to save this file before last commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def is_main_proudction?
-    Rails.application.config.respond_to? 'PRODUCTION_DOMAIN' and Rails.application.config.PRODUCTION_DOMAIN.include? request.host_with_port 
+    Rails.application.config.respond_to? 'PRODUCTION_DOMAINS' and Rails.application.config.PRODUCTION_DOMAINS.include? request.host_with_port 
   end
   
 end
