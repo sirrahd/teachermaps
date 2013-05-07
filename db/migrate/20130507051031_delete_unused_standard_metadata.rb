43950d7989f37d9d2970e630dbc5603da0a51d67
James Kobol <jim.kobol@gmail.com>
2013-05-06 23:18:01 -0600

    Added standard migration to remove CA Content Standard type

---
+++ db/migrate/20130507051031_delete_unused_standard_metadata.rb
@@ -0,0 +1,13 @@
+class DeleteUnusedStandardMetadata < ActiveRecord::Migration
+  def up
+    
+    # Remove CA Content Standards
+    standard = StandardType.find_by_name 'California Content Standards'
+    standard.delete if standard
+
+
+  end
+
+  def down
+  end
+end
