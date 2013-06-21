079ec21653defd7989a385063d2090497bc0cefe
James Kobol <jim.kobol@gmail.com>
2013-06-20 18:58:22 -0600

    removed the update_model code; it was not doing anything

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,12 +47,6 @@ class User < ActiveRecord::Base
 
   validates :password, presence: true, length: { minimum: 6 }, confirmation: true, if: :password_digest_changed?
 
-  after_save :update_model
-
-  def update_model
-
-  end
-
   def has_google_account?
     !google_account.nil? and !google_account.folder_id.nil?
   end
