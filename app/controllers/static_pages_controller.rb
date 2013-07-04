83d198b2752536cbcd3d659f5dcdb08a8bcc21f2
James Kobol <jim.kobol@gmail.com>
2013-07-04 09:47:21 -0600

    added production only robots solution

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+
   def home
     if signed_in?
       redirect_to @current_user
@@ -31,4 +32,13 @@ class StaticPagesController < ApplicationController
 
   def page500
   end
+
+  def robots
+  	if is_main_proudction?
+	  	robots = File.read(Rails.root + "config/robots.production.txt")
+	  else
+	  	robots = File.read(Rails.root + "config/robots.development.txt")
+	  end
+	  render :text => robots, :layout => false, :content_type => "text/plain"
+	end
 end
