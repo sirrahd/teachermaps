ef038f077b626cd1463f81b193023a7717543c1f
James Kobol <jim.kobol@gmail.com>
2013-09-05 21:36:22 -0600

    backup

--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,7 @@ Teachermaps::Application.routes.draw do
   match '/resources/page' => 'resources#page'
   resources :resources
 
-  match 'users/resources/:user_id' => 'resources#index', :as => 'show_resources'
+  match 'users/:user_id/resources' => 'resources#index', :as => 'show_resources'
   resources :users do
     resources :maps
     resources :map_standards, :path => 'standards', only: [:show]
